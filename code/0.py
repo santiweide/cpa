9d265e820e43933ffd7b02c6052ccda68c6e6c86
santiweide <ggy_778@mail.dlut.edu.cn>
2020-03-11 17:54:24 +0800

    conclusion

--- code/0.py
+++ code/0.py
@@ -6,7 +6,7 @@ font = FontProperties(fname=r"C:\Windows\Fonts\simhei.ttf", size=14)
 date = np.array(['Mar-18','Apr-18','May-18','Jun-18','Jul-18','Aug-18','Sep-18','Oct-18','Nov-18','Dec-18','Jan-19','Feb-19']) 
 
 def plt_active():
-	active_ne = np.array([4898.80,3234.60,3159.30,2902.60,2762.40,2883.50,2874.00,3395.00,3176.60,2702.10,2374.20,2469.80])
+	active_ne = np.array([2994.20,3234.60,3159.30,2902.60,2762.40,2883.50,2874.00,3395.00,3176.60,2702.10,2374.20,2469.80])
 	active_qq = np.array([4898.80,5348.10,5188.50,4907.90,4838.70,4941.50,4681.40,5562.00,5132.20,4432.00,4191.40,4697.50])
 	plt.plot(date, active_qq, label='QQ Music',c='g')
 	plt.plot(date, active_ne, label='NetEase Cloud Music',c='r')
@@ -33,7 +33,7 @@ def plt_on():
 	plt.show()
 
 def plt_use():
-	ret_ne = np.array([15.8,21.1,19.3,17.1,16.2,16.1,15.8,16.8,17,16.9,16.8,17.1])
+	ret_ne = np.array([21.3,21.1,19.3,17.1,16.2,16.1,15.8,16.8,17,16.9,16.8,17.1])
 	ret_qq = np.array([15.8,15.4,13.8,13,12.4,12.1,12,11.6,11.6,11.8,12,12.3])
 	plt.plot(date, ret_qq, label='QQ Music',c='g')
 	plt.plot(date, ret_ne, label='NetEase Cloud Music',c='r')
@@ -42,5 +42,6 @@ def plt_use():
 	plt.show()
 
 
+
 if __name__ == '__main__':	
-	
+	plt_use();	
